59588fb61e8fb8b3122bad1b8c5799bfe2e8a4dd
takahiro <metomasskawa@gmail.com>
2019-05-26 11:05:45 +0900

    微調整

--- docker/build.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-docker build \
-    -t ubuntu-jwm \
-    --build-arg DOCKER_UID=$(id -u) \
-    --build-arg DOCKER_USER='gucchy' \
-    --build-arg DOCKER_PASSWORD='password' /vagrant/docker/.
-
